3fa81e8fe8ebb1db439dfd43ff51f77e0dc1cf7e
varshiniselvam2007-cpu <varshiniselvam2007@gmail.com>
2026-08-08 13:25:08 +0530

    Create main.py

---
+++ BASIC WEATHER APP/main.py
@@ -0,0 +1,34 @@
+import requests
+
+city = input("Enter city name: ").strip()
+
+if city == "":
+    print("Error: City name cannot be empty.")
+else:
+    api_key = "YOUR_API_KEY"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200:
+            temp_c = data["main"]["temp"]
+            temp_f = (temp_c * 9/5) + 32
+            humidity = data["main"]["humidity"]
+            weather = data["weather"][0]["description"]
+            wind = data["wind"]["speed"]
+
+            print("\nWeather Details")
+            print("City:", city)
+            print("Temperature:", round(temp_c, 2), "°C")
+            print("Temperature:", round(temp_f, 2), "°F")
+            print("Humidity:", humidity, "%")
+            print("Weather:", weather)
+            print("Wind Speed:", wind, "m/s")
+        else:
+            print("Error:", data.get("message", "City not found"))
+
+    except requests.exceptions.RequestException:
+        print("Network Error. Please try again.")
